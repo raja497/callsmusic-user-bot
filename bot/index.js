f25dd61e21fedd0f425a74947bf816f0b9c219e7
Roj Serbest <49933115+rojserbest@users.noreply.github.com>
2021-06-14 17:01:39 +0300

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -11,12 +11,6 @@ bot.api.config.use((prev, method, payload) => {
     });
 });
 
-bot.catch(async (error) => {
-    if (error.error instanceof Error) {
-        await error.ctx.reply(error.error.toString());
-    }
-});
-
 module.exports.bot = bot;
 module.exports.start = async () => {
     await bot.start({ dropPendingUpdates: true });
